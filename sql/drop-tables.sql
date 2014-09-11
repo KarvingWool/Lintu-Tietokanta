8e5b2b1c089651391b872ca0bf97ced316d4d2bc
xvax <jesse.vaisanen@cs.helsinki.fi>
2014-09-11 21:43:28 +0300

    sql ja html tuunausta

---
+++ sql/drop-tables.sql
@@ -0,0 +1,13 @@
+DROP TABLE LajinAlueet;
+
+DROP TABLE LajienElinympariso;
+
+DROP TABLE Havainto;
+
+DROP TABLE Esiintymisalue;
+
+DROP TABLE Elinymparisto;
+
+DROP TABLE Laji;
+
+DROP TABLE Kayttaja;
