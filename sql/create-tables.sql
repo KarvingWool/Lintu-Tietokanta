9084af3e6a9827f1ddacb2bcdc67c6345d531501
xvax <jesse.vaisanen@cs.helsinki.fi>
2014-09-11 21:44:19 +0300

    sql ja html tuunausta

--- sql/create-tables.sql
+++ sql/create-tables.sql
@@ -4,25 +4,33 @@ esiintymisalue_Alue varchar(30) primary key
 CREATE TABLE Elinymparisto (
 elinymparisto_Elinymparisto varchar(30) primary key
 );
-CREATE TABLE LajinAlueet (
-lajinalueet.laji varchar(30) primary key,
-lajinalueet.alue varchar(30) primary key,
-FOREIGN KEY(lajinalueet_alue) references Esiintymisalue(esiintymisalue_alue),
-FOREIGN KEY(lajinalueet_laji) references Laji(laji_laji)
-);
-
-CREATE TABLE LajinElinymparisto (
-lajinelinymparisto.laji varchar(30) primary key,
-lajinelinymparisto.elinymparisto varchar(30) primary key,
-FOREIGN KEY(elinymparisto_alue) references Elinymparisto(elinymparisto_elinymparisto),
-FOREIGN KEY(lajinelinymparisto_laji) references Laji(laji_laji)
-);
 
 CREATE TABLE Laji (
 Laji_lajinimi varchar(30) primary key,
 laji_latina varchar(30) not null,
 laji_parimaara int,
 laji_lasna varchar(7)
+);
+
+CREATE TABLE Kayttaja (
+kayttaja_kayttajatunnus varchar(32) primary key,
+kayttaja_yllapitaja boolean not null,
+kayttaja_nimi varchar(50) not null,
+kayttaja_sahkoposti varchar(30)
+);
+
+CREATE TABLE LajinAlueet (
+lajinalueet_laji varchar(30) primary key,
+lajinalueet_alue varchar(30) primary key,
+FOREIGN KEY(lajinalueet_alue) references Esiintymisalue(esiintymisalue_alue),
+FOREIGN KEY(lajinalueet_laji) references Laji(laji_laji)
+);
+
+CREATE TABLE LajinElinymparisto (
+lajinelinymparisto_laji varchar(30) primary key,
+lajinelinymparisto_elinymparisto varchar(30) primary key,
+FOREIGN KEY(elinymparisto_alue) references Elinymparisto(elinymparisto_elinymparisto),
+FOREIGN KEY(lajinelinymparisto_laji) references Laji(laji_laji)
 );
 
 CREATE TABLE Havainto (
@@ -40,10 +48,5 @@ FOREIGN KEY(havainto_laji) references laji(laji_laji),
 FOREIGN KEY(havainto_havainnontekijantunnus) references kayttaja(kayttaja_kayttajatunnus)
 );
 
-CREATE TABLE Kayttaja (
-kayttaja_kayttajatunnus varchar(32) primary key,
-kayttaja_yllapitaja boolean not null,
-kayttaja_nimi varchar(50) not null,
-kayttaja_sahkoposti varchar(30)
-);
+
  
